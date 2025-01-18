3c881c3de9798ea58c4838045f196c481d3ecfcd
flaviapl <102832719+flaviapl@users.noreply.github.com>
2025-01-18 15:22:59 +0000

    style added

--- src/js/components/todoList.jsx
+++ src/js/components/todoList.jsx
@@ -27,40 +27,30 @@ function hideX(){
     setHovered(null)
 }
 
-
-
     return (
-        <div className="container" style={{border: "solid black 1px", height:"800px", width:"800px", backgroundColor:"#f5f5f5", marginTop:"5px"}}>
-            <h1 style={{textAlign:"center", fontSize:"80px", fontWeight:"lighter", marginTop:"20px"}}>todos</h1>
-            <ul style={{listStyle:"none"}}>
-                <li><input type="text" value={input} onChange={(e)=>setInput(e.target.value)} onKeyDown={addTask} className="form-control" id="input" style={{border: "solid grey 1px", height:"30px", marginTop:"0px",width:"600px", height:"45px", margin: "0 auto", borderRadius:"0px", backgroundColor:"#f5f5f5", paddingLeft:"60px", fontSize:"20px"}}/></li>
-                {tasks.length === 0 ? (<li style={{border: "solid grey 1px", marginTop:"0px", width:"600px", height:"45px", margin: "0 auto", display:"flex", alignItems: "center", justifyContent:"space-between", paddingLeft: "10px", paddingRight:"10px", fontSize:"20px",}}>
-                    No tasks, add a task
-                </li>
-            ) : (
-                    tasks.map((item, index) => (<li style={{border: "solid grey 1px", marginTop:"0px", width:"600px", height:"45px", margin: "0 auto", display:"flex", alignItems: "center", justifyContent:"space-between", paddingLeft: "60px", paddingRight:"10px", fontSize:"20px"}} onMouseEnter={()=>showX(index)} onMouseLeave={()=>hideX(index)}>{item} {hovered === index && (<i className="fa-solid fa-x" style={{cursor: "pointer", color: "red", paddingRight:"10px"}}onClick={()=>removeTask(index)}></i>
-                )}
-                </li>)))}
-                <p style={{border: "solid grey 1px", marginTop:"0px", width:"600px", height:"45px", margin: "0 auto", display:"flex", alignItems: "center", paddingLeft: "10px", fontSize:"12px", color:"gray"}}>{tasks.length} item left</p>
-            </ul>
-        </div>
+        <>
+            <div className="container" style={{border:"0", height:"650px", width:"800px", backgroundColor:"rgb(243, 243, 243)", marginTop:"25px"}}>
+                <h1 style={{textAlign:"center", fontSize:"105px", fontWeight:"95", marginTop:"30px", color: "rgb(236, 220, 221)", fontFamily:"Roboto"}}>todos</h1>
+                <ul style={{listStyle:"none", position:"relative", zIndex:"2", backgroundColor:"white"}}>
+                    <li><input type="text" value={input} onChange={(e)=>setInput(e.target.value)} onKeyDown={addTask} className="form-control" id="input" placeholder="What needs to be done?" style={{fontFamily:"Roboto", fontWeight:"360", fontSize:"26px", border: "solid rgb(221, 221, 221) 1px", borderBottomWidth:"inherit", marginTop:"0px", width:"600px", height:"65px", margin: "0 auto", borderRadius:"0px", backgroundColor:"rgb(252, 252, 252)", paddingLeft:"60px"}}/></li>
+                    {tasks.length === 0 ? (<li style={{fontFamily:"Roboto", fontWeight:"360", fontSize:"26px", border: "solid rgb(221, 221, 221) 1px", borderBottomWidth:"inherit", marginTop:"0px", width:"600px", height:"65px", margin: "0 auto", display:"flex", alignItems: "center", justifyContent:"space-between", paddingLeft: "10px", paddingRight:"10px", fontSize:"20px",}}>
+                        No tasks, add a task
+                    </li>
+                ) : (
+                        tasks.map((item, index) => (<li style={{fontFamily:"Roboto", fontWeight:"360", fontSize:"26px", border: "solid rgb(221, 221, 221) 1px", marginTop:"0px", width:"600px", height:"65px", margin: "0 auto", display:"flex", alignItems: "center", justifyContent:"space-between", paddingLeft: "60px", paddingRight:"10px", fontSize:"20px"}} onMouseEnter={()=>showX(index)} onMouseLeave={()=>hideX(index)}>{item} {hovered === index && (<i className="fa-solid fa-x" style={{cursor: "pointer", color: "red", paddingRight:"10px"}}onClick={()=>removeTask(index)}></i>
+                    )}
+                    </li>)))}
+                    <p style={{fontFamily:"Roboto", fontWeight:"360", fontSize:"15px", color:"gray", border: "solid rgb(221, 221, 221) 1px", marginTop:"0px", width:"600px", height:"45px", margin: "0 auto", display:"flex", alignItems: "center", paddingLeft: "10px"}}>{tasks.length} item left</p>
+                    <div style={{border: "solid rgb(221, 221, 221) 1px", height:"7px", width:"594px", backgroundColor:"rgb(252, 252, 252)", marginTop:"5px", zIndex:"1", position:"absolute", left:"106px", bottom:"-7px" }}></div>
+                    <div style={{border: "solid rgb(221, 221, 221) 1px", height:"7px", width:"585px", backgroundColor:"rgb(252, 252, 252)", marginTop:"5px", zIndex:"0", position:"absolute", left:"111px", bottom:"-10px"}}></div>
+                </ul>
+
+            </div>
+
+
+        </>
     )
 }
-
-
-
-//PSEUDOCODIGO:
-//CREAR INPUT ok
-// quita label
-//PONER "No hay tareas, añadir tareas" COMO PLACEHOLDER? Cuando no haya tareas pendientes 
-//CREAR VARIABLE GLOBAL PARA LAS TAREAS
-//METER LAS TAREAS DENTRO DE INPUT COMO VALUE?
-//onchange
-//LAS TAREAS SE AÑADE CON ENTER --> onKeyDown {funzione}
-//CREAR ICONO DE ELIMINAR
-//MOSTRAR EL ICONO SOLO CUANDO SE PASE EL RATÇON ENCIMA (HOVER)
-//CREAR LA LISTA PARA LAS TAREAS 
-//LIST.MAP ??
 
 
 export default TodoList
